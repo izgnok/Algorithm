d08b5311ccf4d56352dde7e37a14ccd20cdc3206
izgnok <101163901+izgnok@users.noreply.github.com>
2024-02-18 17:44:23 +0900

    [Gold II] Title: 문제집, Time: 584 ms, Memory: 46164 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static List<List<Integer>> graph = new ArrayList<>();
+	static StringBuilder sb = new StringBuilder();
+
+	// bfs
+	static int[] in_degree;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		// StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
+		}
+
+		in_degree = new int[N + 1];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			graph.get(a).add(b);
+			in_degree[b]++;
+		}
+
+		bfs_sort();
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+	static void bfs_sort() {
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+		for (int i = 1; i <= N; i++) {
+			if (in_degree[i] == 0) {
+				q.add(i);
+			}
+		}
+
+		for (int k = 0; k < N; k++) {
+			if (q.isEmpty()) {
+				return; // 사이클존
+			}
+			int x = q.poll();
+			sb.append(x + " ");
+
+			for (int i = 0; i < graph.get(x).size(); i++) {
+				int next = graph.get(x).get(i);
+				in_degree[next]--;
+				if (in_degree[next] == 0)
+					q.add(next);
+			}
+		}
+	}
+
+}
