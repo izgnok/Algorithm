3fd5a002157b10a32db61fb440978dd26d0e651f
izgnok <101163901+izgnok@users.noreply.github.com>
2024-03-07 16:52:47 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 788 ms, Memory: 130160 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static List<Integer> list;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+
+		list = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if (list.isEmpty()) {
+				list.add(num);
+			} else {
+				int pre = list.get(list.size() - 1);
+				if (pre >= num) {
+					int idx = lowerBound(num);
+					list.set(idx, num);
+				} else {
+					list.add(num);
+				}
+			}
+		}
+		sb.append(list.size());
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+	static int lowerBound(int num) {
+		int start = 0;
+		int end = list.size() - 1;
+
+		while (start < end) {
+			int mid = (start + end) / 2;
+			
+			if(list.get(mid) >= num) {
+				end = mid;
+			}
+			else {
+				start = mid+1;
+			}
+		}
+		return end;
+	}
+
+}
