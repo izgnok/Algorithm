27383461d0f8090ed8311fe326bfa7052c62add0
izgnok <101163901+izgnok@users.noreply.github.com>
2024-04-08 17:08:32 +0900

    [Unrated] Title: [모의 SW 역량테스트] 원자 소멸 시뮬레이션, Time: 2,147 ms, Memory: 140,172 KB -BaekjoonHub

--- SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.java
+++ SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.java
@@ -1,90 +1,79 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int N, result;
-	static int[][] direct = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
+	static int N;
+	static int result;
+	static HashMap<Node, Integer> map;
+	static Queue<Node> q;
+	static int[][] direct = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
+	static boolean death[];
 
-	static HashMap<Node, Integer> visit;
-	static HashSet<Integer> death;
-	static int[] energy_list;
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
-
-		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int T = Integer.parseInt(st.nextToken());
 		for (int test_case = 1; test_case <= T; test_case++) {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
-
-			Queue<Node> q = new ArrayDeque<>();
-			energy_list = new int[N];
+			q = new ArrayDeque<>();
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				int x = (1000 + Integer.parseInt(st.nextToken())) * 2;
-				int y = (1000 + Integer.parseInt(st.nextToken())) * 2;
-				int direct = Integer.parseInt(st.nextToken());
+				int x = Integer.parseInt(st.nextToken()) * 2;
+				int y = Integer.parseInt(st.nextToken()) * 2; // 1번에 0.5초씩 이동하기 위해서, 2배씩 늘려줌
+				int dir = Integer.parseInt(st.nextToken());
 				int energy = Integer.parseInt(st.nextToken());
-				q.add(new Node(i, x, y, direct, energy));
-				energy_list[i] = energy;
-			}
-
-			// 시뮬레이션
-			death = new HashSet<>();
-			while (true) {
-				Queue<Node> tmp = new ArrayDeque<>();
-				visit = new HashMap<>();
-
-				while (!q.isEmpty()) {
-					Node node = q.poll(); // 이동하려는 원소 추출
-
-					int x = node.x + direct[node.direct][1];
-					int y = node.y + direct[node.direct][0];
-					if (death.contains(node.num)) { // 이미 죽은경우
-						continue;
-					}
-
-					if (x < 0 || x >= 4001 || y < 0 || y >= 4001) { // 범위밖으로 벗어난 경우 더 이상 충돌 가능성 없음
-						continue;
-					}
-
-					Node next = new Node(node.num, x, y, node.direct, node.energy);
-
-					if (!visit.containsKey(next)) { // 이동 가능한 경우
-						visit.put(next, node.num);
-						tmp.add(next);
-					} else { // 이동 불가능한 경우 ( 충돌 )
-						int pre = visit.get(next);
-						death.add(pre);
-						death.add(node.num);
-					}
-				}
-				if (tmp.isEmpty()) { // 더이상 충돌가능성이있는 원소가 없는 경우
-					break;
-				}
-				q = tmp;
+				Node node = new Node(x, y, dir, energy, i);
+				q.add(node);
 			}
 
 			result = 0;
-			Iterator<Integer> iter = death.iterator(); // 죽은 세포들의 에너지 합 구하기
-			while (iter.hasNext()) {
-				result += energy_list[iter.next()];
+			death = new boolean[N];
+			while (!q.isEmpty()) { // 충돌가능성이 있는 원소가 남아있을때까지
+				Queue<Node> next = new ArrayDeque<>(); // 다음번 움직임에 충돌 가능성이 있는 원소들
+				map = new HashMap<>(); // 충돌감지 변수
+				int size = q.size(); // 큐 사이즈
+				for (int i = 0; i < size; i++) {
+					Node node = q.poll();
+					if (death[node.num]) {
+						result += node.energy;
+						continue; // 후충돌로 사망한 경우
+					}
+					
+					int row = node.x + direct[node.dir][0];
+					int col = node.y + direct[node.dir][1]; // 이동
+					
+					if (row < -2000 || row > 2000 || col < -2000 || col > 2000) // 충돌 가능성이 사라지는 경우
+						continue;
+
+					if (map.containsKey(new Node(row, col))) { // 충돌하는 경우
+						result += node.energy;
+						death[map.get(new Node(row, col))] = true; // 사망체크
+						death[node.num] = true; // 사망체크
+						continue;
+					}
+					map.put(new Node(row, col), node.num); // 이동체크
+					next.add(new Node(row, col, node.dir, node.energy, node.num)); // 다음에 충돌가능성있음
+				}
+				q = next; // 복사
 			}
 			sb.append("#").append(test_case).append(" ").append(result).append("\n");
 		}
@@ -94,31 +83,45 @@ public class Solution {
 	}
 
 	static class Node {
-		int num, x, y, energy, direct;
+		int x, y, dir, energy, num;
 
-		Node(int num, int x, int y, int direct, int energy) {
-			this.num = num;
+		public Node(int x, int y) {
+			super();
 			this.x = x;
 			this.y = y;
-			this.direct = direct;
+		}
+
+		public Node(int x, int y,  int dir, int energy, int num) {
+			this.x = x;
+			this.y = y;
+			this.dir = dir;
 			this.energy = energy;
+			this.num = num;
+		}
+
+		@Override
+		public String toString() {
+			return "Node [x=" + x + ", y=" + y + ", energy=" + energy + ", dir=" + dir + "]";
 		}
 
 		@Override
 		public int hashCode() {
 			int result = 17;
-			result = 31 * result + x;
-			result = 31 * result + y;
+			result = 31 * result * x;
+			result = 31 * result * y;
 			return result;
 		}
 
 		@Override
 		public boolean equals(Object obj) {
-			if (!(obj instanceof Node)) {
+			if (this == obj)
+				return true;
+			if (obj == null)
 				return false;
-			}
-			Node node = (Node) obj;
-			return this.x == node.x && this.y == node.y;
+			if (getClass() != obj.getClass())
+				return false;
+			Node other = (Node) obj;
+			return x == other.x && y == other.y;
 		}
 	}
 }
