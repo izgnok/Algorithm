df0409ae0366a225c251ccdf22bbcef6b93d0ba9
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-05-08 19:59:17 +0900

    [Silver II] Title: 아기 상어 2, Time: 76 ms, Memory: 12184 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -11,6 +11,9 @@ public class Main {
     static List<Node> sharks;
 
     static int[][] dir = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
+    static boolean[][] visit;
+    static Queue<Node> q = new ArrayDeque<>();
+    static int result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,49 +26,39 @@ public class Main {
         map = new int[N][M];
 
         sharks = new ArrayList<>();
+        visit = new boolean[N][M];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1) {
+                    q.add(new Node(i, j));
+                    visit[i][j] = true;
+                }
             }
         }
 
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 1) continue;
-                pq.add(bfs(new Node(i, j)));
-            }
-        }
-        sb.append(pq.poll());
+        bfs();
+        sb.append(result);
         bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
 
-    static int bfs(Node start) {
-        Queue<Node> q = new ArrayDeque<>();
-        boolean[][] visit = new boolean[N][M];
-        q.add(start);
-        visit[start.x][start.y] = true;
-
+    static void bfs() {
         while (!q.isEmpty()) {
             Node node = q.poll();
+            result = Math.max(node.count, result);
 
             for (int i = 0; i < 8; i++) {
                 int row = node.x + dir[i][0];
                 int col = node.y + dir[i][1];
                 if (row < 0 || row >= N || col < 0 || col >= M) continue;
                 if (visit[row][col]) continue;
-                if (map[row][col] == 1) {
-                    return node.count + 1;
-                }
                 visit[row][col] = true;
                 q.add(new Node(row, col, node.count + 1));
             }
         }
-        return -1;
     }
 
 
