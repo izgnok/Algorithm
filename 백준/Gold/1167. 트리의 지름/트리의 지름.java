0cbec566b97748408a634a98e54b496a9d1bcca3
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-05-09 12:15:41 +0900

    [Gold II] Title: 트리의 지름, Time: 852 ms, Memory: 111432 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -0,0 +1,87 @@
+import org.w3c.dom.Node;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+
+    static List<List<Node>> graph;
+    static int max;
+    static int start;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            while (true) {
+                int b = Integer.parseInt(st.nextToken());
+                if (b == -1) break;
+                int c = Integer.parseInt(st.nextToken());
+                graph.get(a).add(new Node(b, c));
+                graph.get(b).add(new Node(a, c));
+            }
+        }
+
+        // DFS 1
+        max = 0;
+        boolean[] visit1 = new boolean[N + 1];
+        visit1[1] = true;
+        dfs(1, 0, visit1);
+
+        // DFS 2
+        boolean[] visit2 = new boolean[N + 1];
+        max = 0;
+        visit2[start] = true;
+        dfs(start, 0, visit2);
+
+//        max = 0;
+//        boolean[] visit = new boolean[N + 1];
+//        visit[1] = true;
+//        dfs(1, 0, visit);
+//        visit[1] = false;
+//        max = 0;
+//        visit[start] = true;
+//        dfs(start, 0, visit);
+//        visit[start] = false;
+        sb.append(max);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    static void dfs(int vertex, int sum, boolean[] visit) {
+        if (max < sum) {
+            start = vertex;
+            max = sum;
+        }
+        for (Node node : graph.get(vertex)) {
+            if (visit[node.vertex]) continue;
+            visit[node.vertex] = true;
+            dfs(node.vertex, sum + node.cost, visit);
+//            visit[node.vertex] = false;
+        }
+    }
+
+    static class Node {
+        int vertex, cost;
+
+        Node(int vertex, int cost) {
+            this.vertex = vertex;
+            this.cost = cost;
+        }
+    }
+}
