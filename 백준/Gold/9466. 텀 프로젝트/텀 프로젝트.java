2e24433a171d0b1bf74be18da65aa0631092fff5
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-05-10 21:12:44 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1032 ms, Memory: 300952 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -1,15 +1,11 @@
-import org.w3c.dom.Node;
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    static int[] check;
     static int[] arr;
-    static int N;
-    static boolean[] visit;
-    static Deque<Integer> dq = new ArrayDeque<>();
+    static boolean[] visited, finished;
+    static int N, count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,46 +16,41 @@ public class Main {
 
         while (T-- > 0) {
             N = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
             arr = new int[N + 1];
+            visited = new boolean[N + 1];
+            finished = new boolean[N + 1];
+            count = 0;
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= N; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            check = new int[N + 1];
-            visit = new boolean[N + 1];
             for (int i = 1; i <= N; i++) {
-                if (visit[i]) continue;
-                dq = new ArrayDeque<>();
-                dfs(i);
+                if (!visited[i]) dfs(i);
             }
 
-            int count = 0;
-            for (int i = 1; i <= N; i++) {
-                if (check[i] == -1) count++;
-            }
-            sb.append(count).append("\n");
+            sb.append(N - count).append("\n"); // 팀 못된 학생 수
         }
+
         bw.write(sb.toString());
         bw.flush();
-        bw.close();
     }
 
     static void dfs(int cur) {
-        dq.addLast(cur);
-        if (visit[cur]) {
-            while (dq.peekFirst() != cur) {
-                if (check[dq.peekFirst()] == 0) check[dq.pollFirst()] = -1;
-                else break;
+        visited[cur] = true;
+        int next = arr[cur];
+
+        if (!visited[next]) {
+            dfs(next);
+        } else if (!finished[next]) {
+            // 사이클 시작
+            for (int i = next; i != cur; i = arr[i]) {
+                count++;
             }
-            while (!dq.isEmpty()) {
-                if (check[dq.peekFirst()] == 0) check[dq.pollFirst()] = 1;
-                else break;
-            }
-            return;
+            count++; // 자기 자신
         }
-        visit[cur] = true;
-        dfs(arr[cur]);
+
+        finished[cur] = true;
     }
 }
