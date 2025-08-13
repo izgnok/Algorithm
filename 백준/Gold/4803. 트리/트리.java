501b001e67d33f9bc23f92f416e955c19decee9f
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-08-13 17:16:28 +0900

    [Gold IV] Title: 트리, Time: 328 ms, Memory: 53516 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.java
+++ 백준/Gold/4803. 트리/트리.java
@@ -1,77 +1,61 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static List<List<Integer>> graph;
-	static boolean cycle = false;
-	static boolean[] visit;
-	static int[] parent;
-	static int result, count, depth;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
-		int test_case = 1;
-		while(true) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			if(N==0 && M==0) break;
-			graph = new ArrayList<>();
-			for(int i=0; i<=N; i++) { // 그래프 정점개수만큼 초기화
-				graph.add(new ArrayList<>());
-			}
-			for(int i=0; i<M; i++) {
-				st = new StringTokenizer(br.readLine());
-				int n = Integer.parseInt(st.nextToken());
-				int m = Integer.parseInt(st.nextToken());
-				graph.get(n).add(m);
-				graph.get(m).add(n);
-			}
-			visit = new boolean[N+1];
-			parent = new int[N+1];
-			result = 0;
-			for(int i=1; i<=N; i++) {
-				cycle = false;
-				count = 1;
-				depth = 0;
-				bfs(i);
-				if(!cycle && count-1 == depth) result++;
-			}
-			if(result == 0) {
-				sb.append("Case " + test_case + ": No trees.\n");
-			}
-			else if(result == 1) {
-				sb.append("Case " + test_case + ": There is one tree.\n");
-			} 
-			else {
-				sb.append("Case " + test_case + ": A forest of " + result + " trees.\n");
-			}
-			test_case++;
-		}	
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
-	
-	public static void bfs(int idx) {
-		if(visit[idx]) {
-			cycle = true;
-			return;
-		}
-		visit[idx] = true;
 
-		for(int i=0; i<graph.get(idx).size(); i++) {
-			if(parent[idx] == graph.get(idx).get(i)) continue;
-			parent[graph.get(idx).get(i)] = idx;
-			count++;
-			depth++;
-			bfs(graph.get(idx).get(i));
-		}
-	}
+    static int[] parent;
+    static boolean[] cycle;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int test_case = 1;
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            if (N == 0 && M == 0) break;
+
+            parent = new int[N + 1];
+            cycle = new boolean[N + 1];
+            for (int i = 1; i <= N; i++) parent[i] = i;
+
+            while (M-- > 0) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                union(a, b);
+            }
+
+            int result = 0;
+            for (int i = 1; i <= N; i++) {
+                if (parent[i] == i && !cycle[i]) result++;
+            }
+            sb.append("Case ").append(test_case++).append(": ");
+            if (result > 1) sb.append("A forest of ").append(result).append(" trees.");
+            else if (result == 1) sb.append("There is one tree.");
+            else sb.append("No trees.");
+            sb.append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    static int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        parent[b] = a;
+
+        if (a == b) cycle[a] = true;
+        if (cycle[b]) cycle[a] = true;
+    }
 }
