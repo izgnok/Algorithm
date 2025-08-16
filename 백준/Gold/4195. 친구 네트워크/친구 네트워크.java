260c607fcb770ccad919f52cf5633b9e4db16182
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-08-16 17:58:33 +0900

    [Gold II] Title: 친구 네트워크, Time: 764 ms, Memory: 86028 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static HashMap<String, String> map;
+    static HashMap<String, Integer> size;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+
+            int N = Integer.parseInt(br.readLine().trim());
+            map = new HashMap<>();
+            size = new HashMap<>();
+
+            while (N-- > 0) {
+                String cmd = br.readLine();
+                String[] arr = cmd.split(" ");
+
+                for (String str : arr) {
+                    if (!map.containsKey(str)) {
+                        map.put(str, str);
+                        size.put(str, 1);
+                    }
+                }
+
+                int result = union(arr[0], arr[1]);
+                sb.append(result).append("\n");
+            }
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    static String find(String str) {
+        if (!map.get(str).equals(str)) map.put(str, find(map.get(str)));
+        return map.get(str);
+    }
+
+    static int union(String a, String b) {
+        a = find(a);
+        b = find(b);
+        if (a.equals(b)) return size.get(a);
+
+        map.put(b, a);
+        int sizeA = size.get(a);
+        int sizeB = size.get(b);
+        size.put(a, sizeA + sizeB);
+        size.put(b, sizeA + sizeB);
+
+        return size.get(a);
+    }
+}
