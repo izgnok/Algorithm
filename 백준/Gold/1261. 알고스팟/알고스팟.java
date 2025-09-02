d91ea0ea71f8a70dba37d4332df65d0e3e93d487
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-09-02 11:39:07 +0900

    [Gold IV] Title: 알고스팟, Time: 212 ms, Memory: 18752 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -9,8 +9,6 @@ public class Main {
     static int[][] map;
     static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-    static int answer;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -19,56 +17,54 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
 
-        for(int i=0; i<N; i++) {
-            String str = br.readLine();
-            for(int j=0; j<M; j++) {
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine().trim();
+            for (int j = 0; j < M; j++) {
                 map[i][j] = str.charAt(j) - '0';
             }
         }
 
-        bfs();
-        sb.append(answer);
+        int result = dijkstra();
+        sb.append(result);
         bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
 
-    static void bfs() {
-        PriorityQueue<Node> pq = new PriorityQueue<>((Comparator.comparingInt(o -> o.count)));
-        boolean[][] visit = new boolean[N][M];
+    static int dijkstra() {
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.count - o2.count);
+        pq.add(new Node(0, 0, 0));
 
-        pq.add(new Node(0,0,0));
-        visit[0][0] = true;
+        int[][] visit = new int[N][M];
+        for (int i = 0; i < N; i++) Arrays.fill(visit[i], 987654321);
+        visit[0][0] = 0;
 
-        while(!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
             Node node = pq.poll();
+            if (visit[node.x][node.y] < node.count) continue;
+            if (node.x == N - 1 && node.y == M - 1) return node.count;
 
-            for(int i=0; i<4; i++) {
+            for (int i = 0; i < 4; i++) {
                 int row = node.x + dir[i][0];
                 int col = node.y + dir[i][1];
+                int count = node.count;
 
-                if(row < 0 || row >= N || col < 0 || col >= M) continue;
-                if(visit[row][col]) continue;
-
-                if(row == N - 1 && col == M - 1) {
-                    answer = node.count;
-                    return;
-                }
-                visit[row][col] = true;
-                int next_count = node.count;
-                if(map[row][col] == 1) next_count++;
-                pq.add(new Node(row, col, next_count));
+                if (row < 0 || row >= N || col < 0 || col >= M) continue;
+                if (map[row][col] == 1) count++;
+                if (visit[row][col] <= count) continue;
+                visit[row][col] = count;
+                pq.add(new Node(row, col, count));
             }
-
         }
+        return -1;
     }
 
     static class Node {
         int x, y, count;
 
-        Node(int x, int y, int count)  {
+        Node(int x, int y, int count) {
             this.x = x;
             this.y = y;
             this.count = count;
