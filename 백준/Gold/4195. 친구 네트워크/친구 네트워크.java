299f941c4c1eaf1b8c21d9df6941326560ddc79b
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-09-02 17:10:17 +0900

    [Gold II] Title: 친구 네트워크, Time: 668 ms, Memory: 70732 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -1,10 +1,12 @@
+import org.w3c.dom.Node;
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
 
-    static HashMap<String, String> map;
-    static HashMap<String, Integer> size;
+    static Map<String, String> parent;
+    static Map<String, Integer> count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,23 +15,26 @@ public class Main {
 
         int T = Integer.parseInt(br.readLine().trim());
         while (T-- > 0) {
-
             int N = Integer.parseInt(br.readLine().trim());
-            map = new HashMap<>();
-            size = new HashMap<>();
+
+            parent = new HashMap<>();
+            count = new HashMap<>();
 
             while (N-- > 0) {
-                String cmd = br.readLine();
-                String[] arr = cmd.split(" ");
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String x = st.nextToken();
+                String y = st.nextToken();
 
-                for (String str : arr) {
-                    if (!map.containsKey(str)) {
-                        map.put(str, str);
-                        size.put(str, 1);
-                    }
+                if (!parent.containsKey(x)) {
+                    parent.put(x, x);
+                    count.put(x, 1);
+                }
+                if (!parent.containsKey(y)) {
+                    parent.put(y, y);
+                    count.put(y, 1);
                 }
 
-                int result = union(arr[0], arr[1]);
+                int result = union(x, y);
                 sb.append(result).append("\n");
             }
         }
@@ -38,19 +43,19 @@ public class Main {
         bw.close();
     }
 
-    static String find(String str) {
-        if (!map.get(str).equals(str)) map.put(str, find(map.get(str)));
-        return map.get(str);
+    static String find(String x) {
+        if (!x.equals(parent.get(x))) parent.put(x, find(parent.get(x)));
+        return parent.get(x);
     }
 
-    static int union(String a, String b) {
-        a = find(a);
-        b = find(b);
-        if (a.equals(b)) return size.get(a);
+    static int union(String x, String y) {
+        x = find(x);
+        y = find(y);
+        if (x.equals(y)) return count.get(x);
 
-        map.put(b, a);
-        size.put(a, size.get(a) + size.get(b));
+        parent.put(y, x);
+        count.put(x, count.get(x) + count.get(y));
 
-        return size.get(a);
+        return count.get(x);
     }
 }
