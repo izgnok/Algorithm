00adcf260baf479037be62cba4a17063db3f4205
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-09-02 10:42:05 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 272 ms, Memory: 26208 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,97 +1,64 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import org.w3c.dom.Node;
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N, M;
-	static int dist[];
-	static int result;
-	static int parent[];
 
-	public static void main(String[] args) throws Exception {
+    static int start, end;
+    static int[] dir = {-1, 1};
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-		dist = new int[100001];
-		parent = new int[100001];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		dijkstra(N);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
 
-		sb.append(result);
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+        sb.append(bfs());
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 
-	static void dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
-		pq.add(new Node(start, 0));
 
-		boolean[] visit = new boolean[100001];
-		dist[start] = 0;
-		parent[start] = -1;
+    static int bfs() {
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.time - o2.time);
+        pq.add(new Node(start, 0));
+        int[] visit = new int[1000001];
+        Arrays.fill(visit, 987654321);
+        visit[start] = 0;
 
-		while (!pq.isEmpty()) {
-			Node node = pq.poll();
-			int x = node.x;
-			int cost = node.cost;
-			visit[x] = true;
-			if (x == M) {
-				result = cost;
-				break;
-			}
-			int[] direct = { 0, -1, 1 };
-			for (int i = 0; i < 3; i++) {
-				int next;
-				if (i >= 1)
-					next = x + direct[i];
-				else
-					next = x * 2;
-				if (next >= 0 && next <= 100000) {
-					if (!visit[next]) {
-						if (i >= 1) {
-							if (dist[next] > cost + 1) {
-								dist[next] = cost + 1;
-								parent[next] = x;
-								pq.add(new Node(next, dist[next]));
-							}
-						} else {
-							if (dist[next] > cost) {
-								dist[next] = cost;
-								parent[next] = x;
-								pq.add(new Node(next, dist[next]));
-							}
-						}
-					}
-				}
-			}
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            if (node.x == end) return node.time;
 
-		}
-	}
+            for (int i = 0; i < 3; i++) {
+                int nextX = node.x;
+                int nextTime = node.time;
+                if (i < 2) {
+                    nextX += dir[i];
+                    nextTime++;
+                } else nextX *= 2;
+                
+                if (nextX < 0 || nextX >= 100001) continue;
+                if (visit[nextX] <= nextTime) continue;
+                visit[nextX] = nextTime;
+                pq.add(new Node(nextX, nextTime));
+            }
+        }
+        return -1;
+    }
 
-	static class Node {
-		int x, cost;
+    static class Node {
+        int x, time;
 
-		Node(int x, int cost) {
-			this.x = x;
-			this.cost = cost;
-		}
-	}
+        Node(int x, int time) {
+            this.x = x;
+            this.time = time;
+        }
+    }
 }
