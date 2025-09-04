a0a2dcb5f713bb3b43d8b88cc6073e33d15418fb
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-09-04 17:19:11 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 956 ms, Memory: 130652 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,62 +1,48 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import org.w3c.dom.Node;
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	private static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[][] arr;
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-		int[][] input = new int[N+1][N+1];
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				input[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		int[][] DP = new int[N+1][N+1];
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				if (i == 1 && j == 1) {
-					DP[i][j] = input[i][j];
-					continue;
-				} else {
-					if (j != 1) {
-						DP[i][j] += DP[i][j - 1] + input[i][j];
-					} else {
-						DP[i][j] = input[i][j];
-					}
-				}
-			}
-		}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x1 = Integer.parseInt(st.nextToken());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-			int result = 0;
-			for(int k=x2; k>=x1; k--) {
-				result += DP[k][y2];
-				if(y1 >= 1) {
-					result -= DP[k][y1-1];
-				}
-			}
-			sb.append(result).append("\n");
-		}
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+        arr = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            for (int j = 0; j < N; j++) {
+                sum += Integer.parseInt(st.nextToken());
+                arr[i][j] = sum;
+            }
+        }
+
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken()) - 1;
+            int y1 = Integer.parseInt(st.nextToken()) - 1;
+            int x2 = Integer.parseInt(st.nextToken()) - 1;
+            int y2 = Integer.parseInt(st.nextToken()) - 1;
+
+            int result = 0;
+            for (int i = x1; i <= x2; i++) {
+                if (y1 == 0) result += arr[i][y2];
+                else result += arr[i][y2] - arr[i][y1 - 1];
+            }
+            sb.append(result).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
