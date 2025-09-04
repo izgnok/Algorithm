55c4b8dda2b35bc20d15245b373bb15c3ebf18d5
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-09-04 09:15:02 +0900

    [Gold III] Title: 파티, Time: 584 ms, Memory: 55168 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,92 +1,83 @@
-import java.io.*;
+import org.w3c.dom.Node;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	static int N, M, Party;
-	static int[][] cost;
-	static List<List<Node>> graph;
-	static int INF = Integer.MAX_VALUE;
+    static int N, M, target;
+    static List<List<Node>> graph;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		Party = Integer.parseInt(st.nextToken());
-		cost = new int[N + 1][N + 1];
+    static int[][] cost;
 
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				cost[i][j] = INF;
-				if (i == j)
-					cost[i][j] = 0;
-			}
-		}
-		graph = new ArrayList<>();
-		for (int i = 0; i <= N; i++) {
-			graph.add(new ArrayList<>());
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-		while (M-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			graph.get(a).add(new Node(b, cost));
-		}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        target = Integer.parseInt(st.nextToken());
 
-		for (int i = 1; i <= N; i++) {
-			dijkstra(i);
-		}
+        cost = new int[N + 1][N + 1];
 
-		int max = 0;
-		for (int i = 1; i <= N; i++) {
-			if (max < cost[i][Party] + cost[Party][i])
-				max = cost[i][Party] + cost[Party][i];
-		}
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+            Arrays.fill(cost[i], 987654321);
+            cost[i][i] = 0;
+        }
 
-		sb.append(max);
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph.get(a).add(new Node(b, cost));
+        }
 
-	static void dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
-		pq.add(new Node(start, 0));
+        for (int i = 0; i <= N; i++) {
+            dijkstra(i, target);
+            dijkstra(target, i);
+        }
 
-		boolean visit[] = new boolean[N + 1];
+        int max = 0;
+        for (int i = 1; i <= N; i++) max = Math.max(max, cost[i][target] + cost[target][i]);
 
-		while (!pq.isEmpty()) {
-			Node node = pq.poll();
-			if (visit[node.x])
-				continue;
-			visit[node.x] = true;
+        sb.append(max).append("\n");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 
-			for (int i = 0; i < graph.get(node.x).size(); i++) {
-				Node next = graph.get(node.x).get(i);
+    static void dijkstra(int start, int end) {
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+        pq.add(new Node(start, 0));
+        boolean[] visit = new boolean[N + 1];
 
-				if (visit[next.x])
-					continue;
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            if (node.vertex == end) return;
+            if (visit[node.vertex]) continue;
+            visit[node.vertex] = true;
 
-				if (cost[start][next.x] > node.cost + next.cost) {
-					cost[start][next.x] = node.cost + next.cost;
-					pq.add(new Node(next.x, cost[start][next.x]));
-				}
-			}
+            for (Node child : graph.get(node.vertex)) {
+                if (visit[child.vertex]) continue;
+                if (cost[start][child.vertex] <= node.cost + child.cost) continue;
+                cost[start][child.vertex] = node.cost + child.cost;
+                pq.add(new Node(child.vertex, cost[start][child.vertex]));
+            }
+        }
+    }
 
-		}
-	}
+    static class Node {
+        int vertex, cost;
 
-	static class Node {
-		int x, cost;
+        public Node(int v, int c) {
+            vertex = v;
+            cost = c;
+        }
+    }
 
-		Node(int x, int cost) {
-			this.x = x;
-			this.cost = cost;
-		}
-	}
 }
