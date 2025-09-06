6bc200f4ab962a619ff30797cd62482548b15bd1
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-09-06 12:29:46 +0900

    [level 3] Title: 입국심사, Time: 121.20 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,30 +1,25 @@
 import java.util.*;
 
 class Solution {
-    static int N;
-    
     public long solution(int n, int[] times) {
         Arrays.sort(times);
         int size = times.length;
         
         long start = 1;
-        long end = times[size-1] * (long)n;
-        long result = 0;
+        long end = times[size - 1] * (long) n;
+        
         while(start <= end) {
             long mid = (start + end) / 2;
             
             long check = 0;
-            for(long time : times) {
+            for(int time: times) {
                 check += (mid / time);
+                if(check >= n) break;
             }
-
-            if(check >= n) {
-                result = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+            
+            if(check >= n) end = mid - 1;
+            else start = mid + 1;
         }
-        return result;
+        return end + 1;
     }
 }
