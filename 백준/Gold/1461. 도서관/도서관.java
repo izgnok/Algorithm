1ea2c2d344958358c41afa3fc8e14616773ca2d5
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-09-08 17:34:22 +0900

    [Gold IV] Title: 도서관, Time: 72 ms, Memory: 11836 KB -BaekjoonHub

---
+++ 백준/Gold/1461. 도서관/도서관.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        List<Integer> list = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num >= 0) list.add(num);
+            else list2.add(num * -1);
+        }
+        list.sort(Comparator.reverseOrder());
+        list2.sort(Comparator.reverseOrder());
+
+        int result = 0;
+        int max = 0;
+        for (int i = 0; i < list.size(); i += K) {
+            result += list.get(i) * 2;
+            max = Math.max(max, list.get(i));
+        }
+        for (int i = 0; i < list2.size(); i += K) {
+            result += list2.get(i) * 2;
+            max = Math.max(max, list2.get(i));
+        }
+        result -= max;
+
+        sb.append(result);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
