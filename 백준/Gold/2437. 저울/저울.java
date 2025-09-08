8c7afd60270ea19539dee5ec14fa56da8e837d44
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-09-08 14:08:10 +0900

    [Gold II] Title: 저울, Time: 72 ms, Memory: 11932 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+
+        int end = arr[0];
+        if (end > 1) sb.append("1");
+        else {
+            for (int i = 1; i < N; i++) {
+                if (arr[i] != end + 1 && end + 1 - arr[i] <= 0) break;
+                end = end + arr[i];
+            }
+            sb.append(end + 1);
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
