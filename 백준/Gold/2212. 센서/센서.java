174a71c4291321955bc7b529c357f8a8f2f429ad
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-09-08 16:29:51 +0900

    [Gold V] Title: 센서, Time: 268 ms, Memory: 19908 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        if (N <= K) sb.append(0);
+        else {
+            int[] arr = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+            Arrays.sort(arr);
+
+            PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));
+            for (int i = 1; i < N; i++) pq.add(Math.abs(arr[i] - arr[i - 1]));
+            int result = 0;
+            while (K-- > 1) pq.poll();
+            while (!pq.isEmpty()) result += pq.poll();
+            sb.append(result);
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    static class Node {
+        int w, cost;
+
+        Node(int w, int cost) {
+            this.w = w;
+            this.cost = cost;
+        }
+    }
+}
