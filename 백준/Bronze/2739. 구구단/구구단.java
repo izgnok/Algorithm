853b2f55b24bb157723cfef7932740dfc53d3078
꽁지 <101163901+izgnok@users.noreply.github.com>
2025-11-01 21:05:01 +0900

    [Bronze V] Title: 구구단, Time: 64 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= 9; i++) {
+            sb.append(N).append(" * ").append(i).append(" = ").append(N * i).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+}
